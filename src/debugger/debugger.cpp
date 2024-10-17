488f3469f54c8551964cdf7f21be708882ea9b9d
ulastanersoyak <ulassztanersoyak@gmail.com>
2024-10-17 13:59:15 +0300

    linux remote code injection via dlopen fn

--- src/debugger/debugger.cpp
+++ src/debugger/debugger.cpp
@@ -21,6 +21,7 @@
 #include <stdexcept>
 #include <system_error>
 
+#include <dlfcn.h>
 #include <elf.h>
 #include <sys/mman.h>
 #include <sys/ptrace.h>
@@ -39,7 +40,6 @@ debugger::debugger(process &proc, std::optional<std::size_t> timeout)
   const auto threads = proc.threads();
 
   for (const thread &t : threads) {
-    errno = 0;
     if (ptrace(PTRACE_ATTACH, static_cast<std::int32_t>(t.tid()), nullptr,
                nullptr) == -1) {
       throw std::system_error(
@@ -48,7 +48,6 @@ debugger::debugger(process &proc, std::optional<std::size_t> timeout)
     }
     while (true) {
       std::int32_t status{0};
-      errno = 0;
       if (waitpid(static_cast<std::int32_t>(t.tid()), &status, 0) == -1) {
         throw std::system_error(
             errno, std::generic_category(),
@@ -58,7 +57,6 @@ debugger::debugger(process &proc, std::optional<std::size_t> timeout)
         this->suspended_threads.emplace_back(proc.pid(), t.tid());
         break;
       } else {
-        errno = 0;
         if (ptrace(PTRACE_CONT, static_cast<std::int32_t>(t.tid()), nullptr,
                    WSTOPSIG(status)) == -1) {
           throw std::system_error(
@@ -92,7 +90,6 @@ debugger::~debugger() noexcept {
 [[nodiscard]] registers debugger::get_regs(const thread &t) const {
 #ifdef __x86_64__
 #ifdef __linux__
-  errno = 0;
   user_regs_struct regs{};
   if (ptrace(PTRACE_GETREGS, static_cast<std::int32_t>(t.tid()), nullptr,
              &regs) == -1) {
@@ -111,7 +108,6 @@ debugger::~debugger() noexcept {
 void debugger::set_regs(const thread &t, const registers &regs) const {
 #ifdef __x86_64__
 #ifdef __linux__
-  errno = 0;
   if (ptrace(PTRACE_SETREGS, static_cast<std::int32_t>(t.tid()), nullptr,
              &regs.regs) == -1) {
     throw std::system_error(
@@ -163,7 +159,6 @@ void debugger::set_regs(const thread &t, const registers &regs) const {
   // nop -> 90
   // nop -> 90
   const std::uint64_t hook_instr = 0x90909090CC050F;
-  errno = 0;
   // destructive
   if (ptrace(PTRACE_POKETEXT, main_thread_tid, executable_region->begin(),
              hook_instr) == -1) {
@@ -192,7 +187,6 @@ void debugger::set_regs(const thread &t, const registers &regs) const {
   // destructive
   this->set_regs(this->main_thread(), edited_regs);
 
-  errno = 0;
   if (ptrace(PTRACE_CONT, main_thread_tid, 0, 0) == -1) {
     throw std::system_error(
         errno, std::generic_category(),
@@ -236,6 +230,7 @@ void debugger::set_regs(const thread &t, const registers &regs) const {
 }
 
 void debugger::load_library(std::string_view path) const {
+#ifdef __x86_64__
 #ifdef __linux__
   const auto regions = this->proc_.memory_regions();
   using namespace std::literals;
@@ -255,8 +250,10 @@ void debugger::load_library(std::string_view path) const {
   }
   std::stringstream stream{};
   stream << file.rdbuf();
+
   const auto elf_str = stream.str();
   const auto *elf = elf_str.data();
+
   Elf64_Ehdr elf_header{};
   std::memcpy(&elf_header, elf, sizeof(elf_header));
   if (std::memcmp(elf_header.e_ident, ELFMAG, SELFMAG) != 0) [[unlikely]] {
@@ -267,16 +264,19 @@ void debugger::load_library(std::string_view path) const {
   std::vector<Elf64_Shdr> section_headers{elf_header.e_shnum};
   std::memcpy(section_headers.data(), elf + elf_header.e_shoff,
               sizeof(Elf64_Shdr) * section_headers.size());
+  // dynamic symbol section header
   const auto symbols =
       std::ranges::find_if(section_headers, [](const Elf64_Shdr &header) {
         return header.sh_type == SHT_DYNSYM;
       });
+
   if (symbols == std::ranges::cend(section_headers)) [[unlikely]] {
     throw std::system_error(
         errno, std::generic_category(),
         std::format("failed to find symbols in elf file in pid: {}",
                     this->proc_.pid()));
   }
+
   std::vector<Elf64_Sym> syms(symbols->sh_size / sizeof(Elf64_Sym));
   std::memcpy(syms.data(), elf + symbols->sh_offset, symbols->sh_size);
   const auto symbol_str_table = section_headers.at(symbols->sh_link);
@@ -290,10 +290,96 @@ void debugger::load_library(std::string_view path) const {
       break;
     }
   }
+  // inject the .so file's path to the targets memory
   const auto mem_region = this->allocate_memory(4096U);
   auto mem_buffer = read_memory_region(this->proc_, mem_region);
   std::memcpy(mem_buffer.data(), path.data(), path.length());
   write_memory_region(this->proc_, mem_region, mem_buffer);
+
+  const auto stack = this->allocate_memory(4096U);
+
+  const auto main_thread_tid = this->main_thread().tid();
+  const auto saved_regs = this->get_regs(this->main_thread());
+  const auto executable_region =
+      std::ranges::find_if(regions, [](const memory_region &region) {
+        return region.has_permissions(permission::EXECUTE);
+      });
+  if (executable_region == std::ranges::cend(regions)) [[unlikely]] {
+    throw std::range_error(
+        std::format("couldnt find an executable memory region of tid: {}",
+                    main_thread_tid));
+  }
+  errno = 0;
+  const auto instr = ptrace(PTRACE_PEEKTEXT, main_thread_tid,
+                            executable_region->begin(), nullptr);
+  if (errno != 0) {
+    throw std::system_error(
+        errno, std::generic_category(),
+        std::format("failed to read the memory of tid: {}", main_thread_tid));
+  }
+  // assembly for the instructions below
+  // nop -> 90
+  // nop -> 90
+  // call rbx -> FF D3
+  // int3 -> CC
+  // nop -> 90
+  // nop -> 90
+  // nop -> 90
+  const std::uint64_t hook_instr = 0x909090CCD3FF9090;
+  // destructive
+  if (ptrace(PTRACE_POKETEXT, main_thread_tid, executable_region->begin(),
+             hook_instr) == -1) {
+    throw std::system_error(
+        errno, std::generic_category(),
+        std::format("failed to write to the memory of tid: {}",
+                    main_thread_tid));
+  }
+  auto edited_regs{saved_regs};
+  // https://chromium.googlesource.com/chromiumos/docs/+/master/constants/syscalls.md
+  edited_regs.regs.rip = executable_region->begin() + 2;
+  edited_regs.regs.rbx = dlopen_addr;
+  edited_regs.regs.rdi = mem_region.begin();
+  edited_regs.regs.rsi = RTLD_NOW;
+  edited_regs.regs.rsp = stack.begin() + stack.size();
+  edited_regs.regs.rbp = edited_regs.regs.rsp;
+  this->set_regs(this->main_thread(), edited_regs);
+
+  if (ptrace(PTRACE_CONT, main_thread_tid, 0, 0) == -1) {
+    throw std::system_error(
+        errno, std::generic_category(),
+        std::format("failed to continue to tid: {}", main_thread_tid));
+  }
+  std::int32_t wstatus = 0;
+  // wait for a process to hit int3
+  if (waitpid(static_cast<std::int32_t>(main_thread_tid), &wstatus, 0) == -1) {
+    throw std::system_error(
+        errno, std::generic_category(),
+        std::format("failed to wait for tid: {}", main_thread_tid));
+  }
+  // check if given process got any signal other than sigtrap
+  if (WSTOPSIG(wstatus) != SIGTRAP) {
+    throw std::system_error(
+        errno, std::generic_category(),
+        std::format("failed to wait for tid: {}", main_thread_tid));
+  }
+  const auto rs_regs = this->get_regs(this->main_thread());
+  const auto mmap_res = rs_regs.regs.rax;
+  if (reinterpret_cast<void *>(mmap_res) == MAP_FAILED) {
+    throw std::system_error(
+        errno, std::generic_category(),
+        std::format("failed mmap execution for the tid: {}", main_thread_tid));
+  }
+  this->set_regs(this->main_thread(), saved_regs);
+  if (ptrace(PTRACE_POKETEXT, main_thread_tid, executable_region->begin(),
+             instr) == -1) {
+    throw std::system_error(
+        errno, std::generic_category(),
+        std::format("failed to restore instructions back to tid: {}",
+                    main_thread_tid));
+  }
+#endif
+#else
+#error "only x86_64 architecture is supported"
 #endif
 }
 
--- test/test.cpp
+++ test/test.cpp
@@ -3,18 +3,9 @@
 #include <print>
 
 int main() {
-  // auto vi = pp::find_process("vi").at(0);
-  // std::println("vi pid: {}", vi.pid());
-  // auto threads = vi.threads();
-  // pp::debugger deb{std::move(vi)};
-  // [[maybe_unused]] auto x = deb.allocate_memory(100);
-  // std::println("{:x}", x.begin());
-  // using namespace std::chrono_literals;
-  // std::this_thread::sleep_for(2000ms); // for (const auto &thread : threads)
-
   pp::process nano = pp::find_process("nano").at(0);
   std::println("nano pid: {}", nano.pid());
   pp::debugger deb{nano};
-  deb.load_library("TEST_STR");
+  deb.load_library("/tmp/test.so");
   return 0;
 }
