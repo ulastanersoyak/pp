ff4b568da2208c1803259c4e4622910d2d64a315
ulastanersoyak <ulassztanersoyak@gmail.com>
2024-10-21 10:59:47 +0300

    linux func addr cache

--- src/process/function_hook.cpp
+++ src/process/function_hook.cpp
@@ -1,11 +1,21 @@
 #include "process/process.hpp"
 #include "util/read_file.hpp"
+
 #include <algorithm>
 #include <cstring>
-#include <elf.h>
 #include <format>
+#include <string>
+#include <unordered_map>
+
+#ifdef __linux__
+#include <elf.h>
+#else
+#error "only linux is supported"
+#endif
 
 namespace pp {
+
+static std::unordered_map<std::string, std::uintptr_t> addr_cache{};
 
 [[nodiscard]] std::vector<std::string> process::function_names() const {
 #ifdef __linux__
@@ -37,23 +47,24 @@ namespace pp {
                     this->pid()));
   }
   std::vector<std::string> fn_names{};
-
   for (const auto &symbols : matching_symbols) {
     // read symbols
     std::vector<Elf64_Sym> syms(symbols.sh_size / sizeof(Elf64_Sym));
     std::memcpy(syms.data(), elf + symbols.sh_offset, symbols.sh_size);
-
     // get the string table associated with this symbol table
     const auto symbol_str_table = section_headers.at(symbols.sh_link);
     std::vector<char> str_table(symbol_str_table.sh_size);
     std::memcpy(str_table.data(), elf + symbol_str_table.sh_offset,
                 str_table.size());
-
     // extract function names
     for (const auto &sym : syms) {
       const auto name = std::string_view(str_table.data() + sym.st_name);
       if (!name.empty()) {
         fn_names.push_back(name.data());
+        if (!addr_cache.contains(std::string(name))) {
+          const auto addr = this->base_addr() + sym.st_value;
+          addr_cache.insert({std::string(name), addr});
+        }
       }
     }
   }
@@ -66,6 +77,9 @@ namespace pp {
 [[nodiscard]] std::optional<uintptr_t>
 process::func_addr(std::string_view function_name) const {
 #ifdef __linux__
+  if (addr_cache.contains(std::string(function_name))) {
+    return addr_cache.at(std::string(function_name));
+  }
   const auto path = this->exe_path();
   const auto elf_opt = read_elf(path);
   if (!elf_opt.has_value()) {
@@ -74,11 +88,9 @@ process::func_addr(std::string_view function_name) const {
   const auto elf = elf_opt.value().c_str();
   Elf64_Ehdr elf_header{};
   std::memcpy(&elf_header, elf, sizeof(elf_header));
-
   std::vector<Elf64_Shdr> section_headers{elf_header.e_shnum};
   std::memcpy(section_headers.data(), elf + elf_header.e_shoff,
               sizeof(Elf64_Shdr) * section_headers.size());
-
   // symbol section headers
   std::vector<Elf64_Shdr> matching_symbols;
   std::ranges::copy_if(section_headers, std::back_inserter(matching_symbols),
@@ -86,23 +98,20 @@ process::func_addr(std::string_view function_name) const {
                          return header.sh_type == SHT_DYNSYM ||
                                 header.sh_type == SHT_SYMTAB;
                        });
-
   for (const auto &symbols : matching_symbols) {
     // read symbols
     std::vector<Elf64_Sym> syms(symbols.sh_size / sizeof(Elf64_Sym));
     std::memcpy(syms.data(), elf + symbols.sh_offset, symbols.sh_size);
-
     // get the string table associated with this symbol table
     const auto symbol_str_table = section_headers.at(symbols.sh_link);
     std::vector<char> str_table(symbol_str_table.sh_size);
     std::memcpy(str_table.data(), elf + symbol_str_table.sh_offset,
                 str_table.size());
-
     // extract function addresses
     for (const auto &sym : syms) {
       const auto name = std::string_view(str_table.data() + sym.st_name);
+      const auto proc_base_addr = base_addr();
       if (name.contains(function_name)) {
-        const auto proc_base_addr = base_addr();
         uintptr_t func_address = proc_base_addr + sym.st_value;
         return func_address;
       }
--- test/test.cpp
+++ test/test.cpp
@@ -6,9 +6,8 @@ int main() {
   pp::process test_prog = pp::find_process("test_prog").at(0);
   std::println("{}", test_prog.exe_path());
   for (const auto &name : test_prog.function_names()) {
-    std::println("{}", pp::demangle(name));
+    std::println("function named {} is resides at address 0x{:x} ",
+                 pp::demangle(name), test_prog.func_addr(name).value_or(0));
   }
-  const auto addr = test_prog.func_addr("is_args_true");
-  std::println("{:x}", addr.value_or(-313131));
   return 0;
 }
